aac1875bee9a887f64b84e38e30553951c8f4341
Sadrah-Alsabagh <sedrasabbagh33@gmail.com>
2022-11-18 15:20:52 +0000

    tree - challenge and test 02

--- javascript/code-challenges/tree-challenges/challenge02/challenge02.js
+++ javascript/code-challenges/tree-challenges/challenge02/challenge02.js
@@ -1,1 +1,9 @@
-// Write here the code challenge solution
+function treeBreadthFirst(p, q) {
+    if (!p || !q) return false;
+    if (!p && !q) return true;
+    if (p.value !== q.value) return false;
+    return (
+        treeBreadthFirst(p.left, q.left) && treeBreadthFirst(p.right, q.right)
+    );
+}
+module.exports= {treeBreadthFirst};
